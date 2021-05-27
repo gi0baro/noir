c90942a525ab7444ede35568ab1c4dddd52bf777
Giovanni Barillari <giovanni.barillari@gmail.com>
2021-05-27 15:26:33 +0200

    Make adict pickable

--- noir/utils.py
+++ noir/utils.py
@@ -1,8 +1,17 @@
+import copy
+
 from typing import Any, Dict
 
 
 class adict(dict):
-    __getattr__ = dict.__getitem__
+    def __getattr__(self, key: str) -> Any:
+        if key.startswith('__'):
+            raise AttributeError
+        return self[key]
+
+    __getstate__ = lambda self: None
+    __copy__ = lambda self: adict(self)
+    __deepcopy__ = lambda self, memo: adict(copy.deepcopy(dict(self)))
 
 
 def dict_to_adict(obj: Dict[str, Any]) -> adict:
